c33fab0e0064d07cb703fa793514305063570795
Igor Costa Oliveira <igorhawkingg@gmail.com>
2025-07-22 22:31:58 -0300

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <nav class="bg-yellow-400 shadow-md py-4 px-8 flex items-center justify-between transform transition-all duration-300 hover:-translate-y-1 hover:shadow-lg">
-      <Logo class="ml-auto mr-4" />
+      <Logo class="ml-8 mr-4" />
       <div class="flex gap-4">
         <NuxtLink to="/" class="bg-white px-4 py-2 rounded-full text-gray-800 hover:bg-gray-100 transition-all duration-300 hover:shadow-md hover:scale-[1.02] transform hover:translate-y-[-2px]">Produtos</NuxtLink>
         <NuxtLink to="/products/create" class="bg-white px-4 py-2 rounded-full text-gray-800 hover:bg-gray-100 transition-all duration-300 hover:shadow-md hover:scale-[1.02] transform hover:translate-y-[-2px]">Novo Produto</NuxtLink>
